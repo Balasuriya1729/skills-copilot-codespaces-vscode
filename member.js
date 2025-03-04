6dbc825ea85ee80c30431a9d869d1e01d2066f11
Balasuriya1729 <137986823+Balasuriya1729@users.noreply.github.com>
2025-03-04 09:32:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,23 @@
+function skillsMember() {
+  return {
+    title: "Skills",
+    skills: [
+      {
+        name: "HTML5",
+        level: 4,
+      },
+      {
+        name: "CSS3",
+        level: 4,
+      },
+      {
+        name: "JavaScript",
+        level: 3,
+      },
+      {
+        name: "React",
+        level: 3,
+      },
+    ],
+  };
+}
